5317fee47f7ad9ecf61ac564df641167afc71a54
Alexander Tsirel <noma4i@gmail.com>
2015-02-11 16:18:25 +1100

    --wip--

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,21 +1,49 @@
 define windows_clamav (
-  $ensure   = 'present',
+  $ensure   = 'enabled',
+  $scan = 'c:\\',
+  $enable_email = 0,
+  $email_from = undef,
+  $email_to = undef,
+  $smtp_pass = undef,
+  $smtp_user = undef,
+  $smtp_host = undef
 ){
   case $ensure {
     'enabled', 'present': {
+      $clam_path = 'C:\\Program Files (x86)\\ClamWin\\bin'
       download_file { "Download ClamAV Installer" :
-        url                   => 'http://downloads.sourceforge.net/project/clamwin/clamwin/0.98.5/clamwin-0.98.5-setup.exe?r=http%3A%2F%2Fwww.clamwin.com%2Fcontent%2Fview%2F18%2F46%2F&ts=1423624211&use_mirror=jaist',
+        url                   => 'http://bit.ly/1yc4QHW',
         destination_directory => 'c:\ProgramData',
         destination_file      => 'clamav.exe',
+      }->
+      exec { 'Run ClamWin installer':
+        command   => 'iex "c:\ProgramData\clamav.exe /sp- /silent /norestart"',
+        creates  => "${clam_path}\\ClamWin.exe",
+        provider  => powershell,
+      }->
+      file { "${clam_path}\\ClamWin.conf":
+        ensure             => file,
+        source_permissions => ignore,
+        content             => template('windows_clamav/clamwin.conf.erb')
+      }->
+      file { "${clam_path}\\freshclam.conf":
+        ensure             => file,
+        source_permissions => ignore,
+        content             => template('windows_clamav/freshclam.conf.erb')
+      }->
+      scheduled_task { 'ClamAV Scan':
+        ensure    => present,
+        enabled   => true,
+        command   => "${clam_path}\\clamscan.exe",
+        arguments => "${scan} -r",
+        trigger   => {
+          schedule   => daily,
+          every      => 2,            # Specifies every other day. Defaults to 1 (every day).
+          start_date => '2011-08-31', # Defaults to 'today'
+          start_time => '08:00',      # Must be specified
+        }
       }
     }
-    exec { 'cmd /c c:\ProgramData\clamav.exe /sp- /silent /norestart  || exit /b 0':
-      path => $::path,
-      logoutput => false,
-      creates  => 'C:\\Program Files (x86)\\ClamWin\\bin\\ClamWin.exe',
-    }
-
-
     default: {
       fail('Invalid ensure option!\n')
     }
